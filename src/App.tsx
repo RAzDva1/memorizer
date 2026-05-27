15821223615d01614ffeabe2545d0f920151688f
Valentin Goryachev <v.goryachev@predicto.ru>
2026-05-27 14:58:45 +0300

    Make long study answers scrollable

--- src/App.tsx
+++ src/App.tsx
@@ -801,9 +801,11 @@ const StudyView = ({
                     {swipeFeedback === 'remember' ? t('remember') : t('hard')}
                   </span>
                 )}
-                {imageUrl && <img className="mini-image" src={imageUrl} alt="" />}
-                <span className="rich-answer" dangerouslySetInnerHTML={{ __html: sanitizeRichText(current.answerText) }} />
-                {audioUrl && <audio controls src={audioUrl} onClick={(event) => event.stopPropagation()} />}
+                <span className="answer-scroll" onClick={(event) => event.stopPropagation()}>
+                  {imageUrl && <img className="mini-image" src={imageUrl} alt="" />}
+                  <span className="rich-answer" dangerouslySetInnerHTML={{ __html: sanitizeRichText(current.answerText) }} />
+                  {audioUrl && <audio controls src={audioUrl} onClick={(event) => event.stopPropagation()} />}
+                </span>
               </span>
             )}
           </button>
